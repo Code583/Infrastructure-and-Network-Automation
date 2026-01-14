fafa3d8a3bbfb748dd0656f2b917225bfd2eb585
C0D3 <121504471+Code583@users.noreply.github.com>
2026-01-13 21:07:01 -0300

    Add files via upload
    A simple Python automation tool to check website availability.

---
+++ URL Status Checker.py
@@ -0,0 +1,15 @@
+import requests
+
+url = input("URL: ")
+
+if not url.startswith("http"):
+    url = "https://" + url
+
+try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+                print(f"{url} is on-line\n")
+        else:
+                print(f"\n{url} returned error: {response.status_code}")
+except:
+        print(f"{url} failed (Conection error/DNS)")
